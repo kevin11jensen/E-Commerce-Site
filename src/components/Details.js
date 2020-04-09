e856ec8f87106ec29e2f0e41653c1ff23160507f
Kevin Jensen <58477243+kevin11jensen@users.noreply.github.com>
2020-04-09 14:43:42 -0700

    Add files via upload

--- src/components/Details.js
+++ src/components/Details.js
@@ -7,8 +7,15 @@ export default class Details extends Component {
     render() {
         return (
             <ProductConsumer>
-                {(value)=> {
-                    const {id, company, img, info, price, title, inCart } = value.detailProduct;
+                {(value) => {
+                    const {id, 
+                        company, 
+                        img, 
+                        info, 
+                        price, 
+                        title, 
+                        inCart 
+                    } = value.detailProduct;
                     return (
                         <div className = 'container py-5'>
                             {/*title*/}
@@ -18,6 +25,38 @@ export default class Details extends Component {
                                 </div>
                             </div>
                             {/*end title*/}
+                            {/*product info*/}
+                            <div className = 'row'>
+                                <div className = 'col-10 mx-auto col-md-6 my-3'>
+                                    <img src = {img} className = 'img-fluid' alt = 'product' />
+                                </div>
+                                {/*product text*/}
+                                <div className = 'col-10 mx-auto col-md-6 my-3 text-capitalize'>
+                                    <h2>model : {title}</h2>
+                                    <h4 className = 'text-title text-uppercase text-muted mt-3 mb-2'>made by : <span className = 'text-uppercase'>{company}</span></h4> 
+                                    <h4 className = 'text-blue'><strong>price : <span>$</span>{price}</strong></h4> 
+                                    <p className = 'text-capitalize font-weight-bold mt-3 mb-0'>some info about the product:</p>  
+                                    <p className = 'text-muted lead'>
+                                        {info}
+                                    </p>
+                                    {/* buttons */}
+                                    <div>
+                                        <Link to = '/'>
+                                            <ButtonContainer>
+                                                back to products
+                                            </ButtonContainer>
+                                        </Link>
+                                        <ButtonContainer 
+                                        cart
+                                        disabled = {inCart ? true : false}
+                                        onClick = {() => {
+                                            value.addToCart(id);
+                                        }}>
+                                            {inCart ? 'inCart' : 'add to cart'}
+                                        </ButtonContainer>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     )
                 }}
--- src/context.js
+++ src/context.js
@@ -11,42 +11,58 @@ class ProductProvider extends Component {
     state = {
         products: [],
         detailProduct: detailProduct
-      }
-      componentDidMount() {
-          this.setProducts();
-      }
-      setProducts = () => {
-          let tempProducts = [];
-          storeProducts.forEach(item => {
-              const singleItem = {...item};
-              tempProducts = [...tempProducts, singleItem];
-
-          })
-          this.setState(() => {
-              return {products:tempProducts}
-          })
-      }
-      handleDetail = () => {
-          console.log('hello from detail');
-      }
-      addToCart = () => {
-          console.log('hello from add to cart');
-      }
+    }
     
+    componentDidMount() {
+        this.setProducts();
+    }
 
+    setProducts = () => {
+        let tempProducts = [];
+        storeProducts.forEach(item => {
+            const singleItem = {...item};
+            tempProducts = [...tempProducts, singleItem];
+
+        })
+        this.setState(() => {
+            return {products:tempProducts}
+        })
+    }
+
+    getItem = id => {
+        const product = this.state.products.find(item => item.id === id);
+    }  
+
+    handleDetail = id => {
+        const product = this.getItem(id);
+        this.setState(() => {
+            return {detailProduct:product}
+        }) 
+    };
+
+    addToCart = id => {
+        let tempProducts = [...this.state.products];
+
+    };
+    
     render() {
         return (
-            <ProductContext.Provider value = {{
-                ...this.state,
-                handleDetail: this.handleDetail,
-                addToCart: this.addToCart,
-            }}>
-               
-                {this.props.children}
-            </ProductContext.Provider>       
-        )
+            <ProductContext.Provider
+                value = {{
+                    ...this.state,
+                    handleDetail: this.handleDetail,
+                    addToCart: this.addToCart
+
+                }}
+                >
+                    {this.props.children}
+            </ProductContext.Provider>
+        );
     }
 }
+
+    
+
 
 const ProductConsumer = ProductContext.Consumer;
 
